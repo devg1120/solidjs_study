2744cc5a7cdc9e7e2e3fc0aa4655e5249852cefc
西草昌市 <nishigusa@nishisoumasashinoMacBook-Pro.local>
2024-01-08 18:33:02 +0900

    up

--- solid-bootstrop/AdminLTE_boostrap5/AdminLTE-astro/astro.config.mjs
+++ solid-bootstrop/AdminLTE_boostrap5/AdminLTE-astro/astro.config.mjs
@@ -2,7 +2,16 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-     srcDir: './src/html'
-
-
+     srcDir: './src/html',
+/*
+vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "src/scss/adminlte.scss";`
+        }
+      }
+    }
+  },
+*/
 });
